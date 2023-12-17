29330e260a8adc9c6cde96848ee5a282b3a8b822
albinols <sopranos31AO>
2023-12-17 21:33:07 +0100

    ändrat till progressbar och lagt till highscore

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ const welcomeElement = document.querySelector('.welcome');
 const quizContainer = document.querySelector('.quiz-container');
 const timerContainer = document.querySelector('.timer-container');
 const timerElement = document.querySelector('.timer');
-const highscoreContainer = document.querySelector('.highscore-container')
-const highscoreElement = document.querySelector('.highscore')
+const progressContainer = document.querySelector('.progress-container');
+const progressBar = document.querySelector('.progress-bar');
 
 let currentQuestionIndex = 0;
 let score = 0;
@@ -19,12 +19,12 @@ let selectedCategory = null;
 let isAnswerSelected = false;
 let selectedCategoryQuestions = [];
 let timer;
+let highscore = 0;
+
 
 function showCategories() {
   buttonContainer.innerHTML = '';
-
   welcomeElement.innerHTML = 'Välkommen'
-
   document.querySelector('.question').innerHTML = 'Välj kategori';
 
   categories.forEach(category => {
@@ -39,10 +39,14 @@ function showCategories() {
     });
     buttonContainer.appendChild(button);
   });
+
+  resetGame();
+
 }
 
 function showQuestion() {
-
+  highscore = localStorage.getItem(`${selectedCategory}-highscore`);
+  
   clearTimeout(timer);
   if (selectedCategory === null) {
     console.error('No category selected.');
@@ -65,26 +69,51 @@ function showQuestion() {
       buttonContainer.appendChild(button);
     });
 
+    progressContainer.classList.remove('hidden');
     timerContainer.classList.remove('hidden');
-    let timeRemaining = 5;
-    const timerElement = document.querySelector('.timer');
-    timerElement.innerText = `Tid: ${timeRemaining}s`;
-
-    timer = setInterval(() => {
-      timeRemaining--;
-      timerElement.innerText = `Tid: ${timeRemaining}s`;
-
-      if (timeRemaining === 0) {
-        clearInterval(timer);
-        handleTimeout(currentQuestion.correctAnswer);
-      }
-    }, 1000);
+    startTimer(5, () => handleTimeout(currentQuestion.correctAnswer));
 
   } else {
     endQuiz();
   }
 }
 
+function startTimer(duration, callback) {
+  let timeRemaining = duration;
+
+  const progressBar = document.querySelector('.progress-bar');
+  progressBar.style.animation = 'none'; // Reset the animation
+
+  // Force a reflow to apply the 'none' style and avoid a flicker
+  progressBar.offsetHeight;
+
+  progressBar.style.animation = `countdown ${duration}s linear`; // Set the animation with the new duration
+
+  timer = setInterval(() => {
+    timeRemaining--;
+    if (timeRemaining === 0) {
+      clearInterval(timer);
+      callback();
+    }
+  }, 1000);
+}
+
+/*
+function startTimer(duration, callback) {
+  let timeRemaining = duration;
+  timerElement.innerText = `Tid: ${timeRemaining}s`;
+
+  timer = setInterval(() => {
+    timeRemaining--;
+    timerElement.innerText = `Tid: ${timeRemaining}s`;
+
+    if (timeRemaining === 0) {
+      clearInterval(timer);
+      callback();
+    }
+  }, 1000);
+}
+*/
 function handleTimeout(correctAnswer) {
   clearInterval(timer);
 
@@ -105,7 +134,6 @@ function handleTimeout(correctAnswer) {
 }
 
 function checkAnswer(selectedOption, correctAnswer) {
-
   if (isAnswerSelected) {
     return;
   }
@@ -137,6 +165,7 @@ function checkAnswer(selectedOption, correctAnswer) {
   setTimeout(() => {
     if (isCorrect) {
       score++;
+      updateHighscore(); 
     }
 
     selectedOption.classList.remove('correct', 'incorrect'); 
@@ -153,6 +182,13 @@ function checkAnswer(selectedOption, correctAnswer) {
   }, 1000); 
 }
 
+function updateHighscore() {
+  if (score > highscore) {
+    highscore = score;
+    localStorage.setItem(`${selectedCategory}-highscore`, highscore);
+  }
+}
+
 function updateQuestionNumber(totalQuestions) {
   const questionNumberElement = document.querySelector('.welcome');
   if (questionNumberElement) {
@@ -160,12 +196,26 @@ function updateQuestionNumber(totalQuestions) {
   }
 }
 
+function resetGame() {
+  currentQuestionIndex = 0;
+  score = 0;
+  selectedCategory = null;
+  selectedCategoryQuestions = [];
+  clearTimeout(timer);
+  timerElement.innerHTML = 'Tid: 5s';
+  document.querySelector('.timer-container').classList.add('hidden');
+}
+
 function endQuiz() {
 
-  timerContainer.classList.add('hidden');
+  progressBar.style.animation = 'none';
+  progressBar.offsetHeight; 
+  progressBar.style.animation = '';
+  progressContainer.classList.add('hidden');
 
+  timerContainer.classList.add('hidden');
   buttonContainer.innerHTML = '';
-  questionElement.innerHTML = `Du fick: ${score} poäng`;
+  questionElement.innerHTML = `Du fick: ${score} poäng. <br> Highscore för ${selectedCategory}: ${highscore} poäng.`;
 
   buttonContainer.classList.add('play-again-button-container');
 
@@ -173,16 +223,10 @@ function endQuiz() {
   playAgainButton.classList.add('play-again-button');
   playAgainButton.innerText = 'Spela igen';
   playAgainButton.addEventListener('click', () => {
-
-    currentQuestionIndex = 0;
-    score = 0;
-    selectedCategory = null;
-    selectedCategoryQuestions = [];
-    clearTimeout(timer);
-    document.querySelector('.timer').innerHTML = 'Tid: 5s'
     showCategories();
   });
   buttonContainer.appendChild(playAgainButton);
 }
 
+updateHighscore();
 showCategories();
