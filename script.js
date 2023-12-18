27b89ae39119b848df1c2bd4d1e865797440be2c
Shedcape <shedcapeyt@gmail.com>
2023-12-18 10:59:32 +0100

    Lagt till state och fixat high score

--- script.js
+++ script.js
@@ -3,23 +3,21 @@ import RandomizeOrder from './RandomizeArray.js';
 
 const categories = ["Historia", "Sport", "Vetenskap", "Geografi"];
 
-const categoryContainer = document.querySelector('.category-container'); 
 const buttonContainer = document.querySelector('.button-container');
 const questionElement = document.querySelector('.question');
 const welcomeElement = document.querySelector('.welcome');
-const quizContainer = document.querySelector('.quiz-container');
-//const timerContainer = document.querySelector('.timer-container');
-//const timerElement = document.querySelector('.timer');
 const progressContainer = document.querySelector('.progress-container');
 const progressBar = document.querySelector('.progress-bar');
 
-let currentQuestionIndex = 0;
-let score = 0;
-let selectedCategory = null;
-let isAnswerSelected = false;
-let selectedCategoryQuestions = [];
-let timer;
-let highscore = 0;
+const state = {
+  currentQuestionIndex: 0,
+  score: 0,
+  selectedCategory: null,
+  isAnswerSelected: false,
+  selectedCategoryQuestions: [],
+  timer: undefined,
+  highscore: 0,
+}
 
 
 function showCategories() {
@@ -32,9 +30,9 @@ function showCategories() {
     button.classList.add('buttons')
     button.innerText = category;
     button.addEventListener('click', () => {
-      selectedCategory = category;
-      const retrievedQuestions = quizData.filter(q => q.category === selectedCategory);
-      selectedCategoryQuestions = RandomizeOrder(retrievedQuestions);    
+      state.selectedCategory = category;
+      const retrievedQuestions = quizData.filter(q => q.category === state.selectedCategory);
+      state.selectedCategoryQuestions = RandomizeOrder(retrievedQuestions);    
       showQuestion();
     });
     buttonContainer.appendChild(button);
@@ -45,19 +43,19 @@ function showCategories() {
 }
 
 function showQuestion() {
-  highscore = localStorage.getItem(`${selectedCategory}-highscore`) || 0;
-  console.log(highscore);
+  state.highscore = localStorage.getItem(`${state.selectedCategory}-highscore`) || 0;
+  console.log(state.highscore);
   
-  clearTimeout(timer);
-  if (selectedCategory === null) {
+  clearTimeout(state.timer);
+  if (state.selectedCategory === null) {
     console.error('No category selected.');
     return;
   }
 
-  if (currentQuestionIndex < selectedCategoryQuestions.length) {
-    const currentQuestion = selectedCategoryQuestions[currentQuestionIndex];
+  if (state.currentQuestionIndex < state.selectedCategoryQuestions.length) {
+    const currentQuestion = state.selectedCategoryQuestions[state.currentQuestionIndex];
 
-    updateQuestionNumber(selectedCategoryQuestions.length);
+    updateQuestionNumber(state.selectedCategoryQuestions.length);
 
     questionElement.innerHTML = currentQuestion.question;
 
@@ -71,7 +69,6 @@ function showQuestion() {
     });
 
     progressContainer.classList.remove('hidden');
-    //timerContainer.classList.remove('hidden');
     startTimer(5, () => handleTimeout(currentQuestion.correctAnswer));
 
   } else {
@@ -89,33 +86,17 @@ function startTimer(duration, callback) {
 
   progressBar.style.animation = `countdown ${duration}s linear`; 
 
-  timer = setInterval(() => {
+  state.timer = setInterval(() => {
     timeRemaining--;
     if (timeRemaining === 0) {
-      clearInterval(timer);
+      clearInterval(state.timer);
       callback();
     }
   }, 1000);
 }
 
-/*
-function startTimer(duration, callback) {
-  let timeRemaining = duration;
-  timerElement.innerText = `Tid: ${timeRemaining}s`;
-
-  timer = setInterval(() => {
-    timeRemaining--;
-    timerElement.innerText = `Tid: ${timeRemaining}s`;
-
-    if (timeRemaining === 0) {
-      clearInterval(timer);
-      callback();
-    }
-  }, 1000);
-}
-*/
 function handleTimeout(correctAnswer) {
-  clearInterval(timer);
+  clearInterval(state.timer);
 
   const correctOptionButton = Array.from(buttonContainer.children).find(button =>
     button.innerText === correctAnswer
@@ -126,19 +107,19 @@ function handleTimeout(correctAnswer) {
 
     setTimeout(() => {
       correctOptionButton.classList.remove('flash-correct');
-      isAnswerSelected = false;
-      currentQuestionIndex++;
+      state.isAnswerSelected = false;
+      state.currentQuestionIndex++;
       showQuestion();
     }, 1000);
   }
 }
 
 function checkAnswer(selectedOption, correctAnswer) {
-  if (isAnswerSelected) {
+  if (state.isAnswerSelected) {
     return;
   }
 
-  isAnswerSelected = true; 
+  state.isAnswerSelected = true; 
 
   const isCorrect = selectedOption.innerText === correctAnswer;
 
@@ -164,58 +145,60 @@ function checkAnswer(selectedOption, correctAnswer) {
 
   setTimeout(() => {
     if (isCorrect) {
-      score++;
-      updateHighscore(); 
+      state.score++;
+
     }
 
     selectedOption.classList.remove('correct', 'incorrect'); 
-    currentQuestionIndex++;
+    state.currentQuestionIndex++;
 
      document.querySelectorAll('.buttons').forEach(button => {
       button.disabled = false;
       button.classList.remove('disabled');
     });
 
-    isAnswerSelected = false;
+    state.isAnswerSelected = false;
 
     showQuestion();
   }, 1000); 
 }
 
-function updateHighscore() {
-  if (score > highscore) {
-    highscore = score;
-    localStorage.setItem(`${selectedCategory}-highscore`, highscore);
+function updateHighscore(reset = false) {
+  if (state.score > state.highscore) {
+    state.highscore = state.score;
+    localStorage.setItem(`${state.selectedCategory}-highscore`, state.highscore);
   }
+  if (reset) {
+    state.highscore = 0;
+    localStorage.setItem(`${state.selectedCategory}-highscore`, state.highscore);
+  }
+  questionElement.innerHTML = `Du fick: ${state.score} poäng. <br> Highscore för ${state.selectedCategory}: ${state.highscore} poäng.`;
 }
 
 function updateQuestionNumber(totalQuestions) {
   const questionNumberElement = document.querySelector('.welcome');
   if (questionNumberElement) {
-    questionNumberElement.textContent = `Fråga: ${currentQuestionIndex + 1}/${totalQuestions}`;
+    questionNumberElement.textContent = `Fråga: ${state.currentQuestionIndex + 1}/${totalQuestions}`;
   }
 }
 
 function resetGame() {
-  currentQuestionIndex = 0;
-  score = 0;
-  selectedCategory = null;
-  selectedCategoryQuestions = [];
-  clearTimeout(timer);
-  //timerElement.innerHTML = 'Tid: 5s';
+  state.currentQuestionIndex = 0;
+  state.score = 0;
+  state.selectedCategory = null;
+  state.selectedCategoryQuestions = [];
+  clearTimeout(state.timer);
   document.querySelector('.timer-container').classList.add('hidden');
 }
 
 function endQuiz() {
-
+  updateHighscore(); 
   progressBar.style.animation = 'none';
   progressBar.offsetHeight; 
   progressBar.style.animation = '';
   progressContainer.classList.add('hidden');
-
-  //timerContainer.classList.add('hidden');
   buttonContainer.innerHTML = '';
-  questionElement.innerHTML = `Du fick: ${score} poäng. <br> Highscore för ${selectedCategory}: ${highscore} poäng.`;
+  
 
   buttonContainer.classList.add('play-again-button-container');
 
@@ -231,7 +214,7 @@ function endQuiz() {
   resetHighscore.classList.add('buttons');
   resetHighscore.innerText = 'Återställ highscore';
   resetHighscore.addEventListener('click', () => {
-  localStorage.removeItem(`${selectedCategory}-highscore`);
+    updateHighscore(true);
   });
   buttonContainer.appendChild(resetHighscore);
 }
